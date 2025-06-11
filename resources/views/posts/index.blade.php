ad707228bc53346d9dde4239bbf54a48cd49d641
Koki715 <foreverbrightness715@gmail.com>
2025-06-11 21:33:20 +0900

    Add files via upload

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,38 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ja">
 
 <head>
-    <title>laravel_kadai</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Laravel基礎</title>
 </head>
 
 <body>
-    <h1>投稿一覧</h1>
+   <h1>投稿一覧</h1><br>
+
+   <table border="0" cellpadding="10">
+       <thead>
+           <tr>
+               <th>タイトル</th>
+               <th>本文</th>
+           </tr>
+       </thead>
+       <tbody>
+           @if ($posts->isEmpty())
+               <tr>
+                   <td colspan="2">投稿がありません。</td>
+               </tr>
+           @else
+               @foreach ($posts as $post)
+                   <tr>
+                       <td>{{ $post->title }}</td>
+                       <td>{{ $post->content }}</td>
+                   </tr>
+               @endforeach
+           @endif
+       </tbody>
+   </table>
+
 </body>
+
 </html>
--- storage/framework/views/5ce2e81f26bae5f4e72e775131caa5a2.php
+++ storage/framework/views/5ce2e81f26bae5f4e72e775131caa5a2.php
@@ -1,11 +1,39 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ja">
 
 <head>
-    <title>laravel_kadai</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Laravel基礎</title>
 </head>
 
 <body>
-    <h1>投稿一覧</h1>
+   <h1>投稿一覧</h1><br>
+
+   <table border="0" cellpadding="10">
+       <thead>
+           <tr>
+               <th>タイトル</th>
+               <th>本文</th>
+           </tr>
+       </thead>
+       <tbody>
+           <?php if($posts->isEmpty()): ?>
+               <tr>
+                   <td colspan="2">投稿がありません。</td>
+               </tr>
+           <?php else: ?>
+               <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                   <tr>
+                       <td><?php echo e($post->title); ?></td>
+                       <td><?php echo e($post->content); ?></td>
+                   </tr>
+               <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+           <?php endif; ?>
+       </tbody>
+   </table>
+
 </body>
-</html><?php /**PATH /Applications/MAMP/htdocs/laravel-basic-kadai/resources/views/posts/index.blade.php ENDPATH**/ ?>
+
+</html>
+<?php /**PATH /Applications/MAMP/htdocs/laravel-basic-kadai/resources/views/posts/index.blade.php ENDPATH**/ ?>
