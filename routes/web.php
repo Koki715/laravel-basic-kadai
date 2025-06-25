fa686bae1a2530cc00df2f55e2b8ce914b2d6fe3
Koki715 <foreverbrightness715@gmail.com>
2025-06-25 20:03:31 +0900

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
@@ -15,11 +16,26 @@ use App\Models\Post;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/create',[PostController::class, 'create'])->name('create');
+Route::get('/posts/create',[PostController::class, 'create'])->name('create')->middleware('auth');;
 Route::get('/posts/{id}', [PostController::class, 'show']);
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create')->middleware('auth');;
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store')->middleware('auth');;
 Route::get('/check', function() {
     return Post::all();
 });
